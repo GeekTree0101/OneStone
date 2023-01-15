054a4c4fc41f9d66d654d4737547a743e905c832
david <h2s1880@gmail.com>
2023-01-15 23:10:59 +0900

    fix: remove a MainActor

--- Sources/OneStone/LiveViewCoordinator.swift
+++ Sources/OneStone/LiveViewCoordinator.swift
@@ -40,7 +40,6 @@ struct OneStoneLogger {
 private let logger = OneStoneLogger(subsystem: "LiveViewNative", category: "LiveViewCoordinator")
 
 /// The live view coordinator object handles connecting to Phoenix LiveView on the backend, managing the websocket connection, and transmitting/handling events.
-@MainActor
 public class LiveViewCoordinator<R: CustomRegistry>: ObservableObject {
     @Published internal private(set) var internalState: InternalState = .notConnected(reconnectAutomatically: false)
     /// The current state of the live view connection.
